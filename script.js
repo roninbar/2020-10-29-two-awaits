3b196fd0238cb12f9158f88812b4b28643d52b96
ron <rinbar70@gmail.com>
2020-10-29 12:29:04 +0200

    Generate a list of countries using "await."

---
+++ script.js
@@ -0,0 +1,21 @@
+async function fetchAllCountriesAsync() {
+    const response = await fetch('https://restcountries.eu/rest/v2/all');
+    const { status, statusText } = response;
+    if (200 <= status && status < 300) {
+        return await response.json();
+    }
+    else {
+        throw new Error(`${status} ${statusText}`);
+    }
+}
+
+window.addEventListener('load', async function () {
+    const countries = await fetchAllCountriesAsync();
+    const lis = countries.map(({ name }) => {
+        const li = document.createElement('li');
+        li.innerText = name;
+        return li;
+    });
+    document.querySelector('ul').append(...lis);
+});
+
