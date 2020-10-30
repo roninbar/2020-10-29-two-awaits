48d80f2c2c9c6c05c852b5365983d1fe27a0129f
ron <rinbar70@gmail.com>
2020-10-30 02:20:16 +0200

    Catch errors.

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-function fetchAllCountries() {
-    return fetch('https://restcountries.eu/rest/v2/all').then(function (response) {
-        const { status, statusText } = response;
-        if (200 <= status && status < 300) {
-            return response.json();
-        }
-        else {
-            throw new Error(`${status} ${statusText}`);
-        }
-    });
-}
-
 async function fetchAllCountriesAsync() {
     const response = await fetch('https://restcountries.eu/rest/v2/all');
     const { status, statusText } = response;
@@ -25,27 +13,42 @@ window.addEventListener('load', function () {
 
     const ul = document.querySelector('ul');
 
+    function createLi(text) {
+        const li = document.createElement('li');
+        li.innerText = text;
+        return li;
+    }
+
     function populateList(countries) {
         ul.innerHTML = '';
-        const lis = countries.map(function ({ name }) {
-            const li = document.createElement('li');
-            li.innerText = name;
-            return li;
-        });
-        ul.append(...lis);
+        ul.append(...countries.map(({ name }) => createLi(name)));
     }
 
     document.getElementById('async').addEventListener('click', async function () {
-        populateList(await fetchAllCountriesAsync());
+        try {
+            populateList(await fetchAllCountriesAsync());
+        } catch (error) {
+            alert(error);
+        }
     });
 
     document.getElementById('then').addEventListener('click', function () {
-        fetchAllCountries().then(function (countries) {
+        fetch('https://restcountries.eu/rest/v2/all').then(function (response) {
+            const { status, statusText } = response;
+            if (200 <= status && status < 300) {
+                return response.json();
+            }
+            else {
+                throw new Error(`${status} ${statusText}`);
+            }
+        }).then(function (countries) {
             populateList(countries);
+        }).catch(function (error) {
+            alert(error);
         });
     });
 
-    document.getElementById('clear').addEventListener('click', function() {
+    document.getElementById('clear').addEventListener('click', function () {
         ul.innerHTML = '';
     });
 });
